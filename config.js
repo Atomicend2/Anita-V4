407d129f3c11ca3bd085fc11c73b6b3c8ebba5fa
Atomicend2 <mrmaize2@gmail.com>
2025-07-07 08:34:40 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,11 @@ const toBool = (value) => value === "true";
 
 // Owner & Bot Details
 global.owner = process.env.OWNER_NUMBER;
-global.anitav4 = process.env.BOT_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
-global.ownername = process.env.OWNER_NAME || "David Cyril";
-global.packname = process.env.PACK_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
-global.author = process.env.AUTHOR || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
-global.channelname = process.env.CHANNEL_NAME || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
+global.anitav4 = process.env.BOT_NAME || "Eden";
+global.ownername = process.env.OWNER_NAME || "Atomic";
+global.packname = process.env.PACK_NAME || "Eden";
+global.author = process.env.AUTHOR || "Atomic";
+global.channelname = process.env.CHANNEL_NAME || "Atomicend";
 global.channeljid = process.env.CHANNEL_JID || "120363315231436175@newsletter";
 
 // Auto-Features
